502a3c773b79f7a7e22d929c7651b20937c60825
Raffaele Valenti <raffaele-valenti@tutamail.com>
2023-06-09 00:11:59 +0200

    Small fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,13 @@ import * as tsconfig from "./tsconfig.paths.json"
 const readAliasFromTsConfig = (): Alias[] => {
   const pathReplaceRegex = new RegExp(/\/\*$/, "")
 
-  return Object.entries(tsconfig.compilerOptions.paths).reduce(
-    (aliases, [fromPaths, toPaths]) => {
-      const find = fromPaths.replace(pathReplaceRegex, "")
-      const toPath = toPaths[0].replace(pathReplaceRegex, "")
-      const replacement = path.resolve(__dirname, toPath)
-      aliases.push({ find, replacement })
-      return aliases
-    },
-    [] as Alias[]
-  )
+  return Object.entries(tsconfig.compilerOptions.paths).reduce((aliases, [fromPaths, toPaths]) => {
+    const find = fromPaths.replace(pathReplaceRegex, "")
+    const toPath = toPaths[0].replace(pathReplaceRegex, "")
+    const replacement = path.resolve(__dirname, toPath)
+    aliases.push({ find, replacement })
+    return aliases
+  }, [] as Alias[])
 }
 
 export default defineConfig({
