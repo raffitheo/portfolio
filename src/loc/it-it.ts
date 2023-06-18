4aabff7164ff2a40b05c5be037ff2754a94656ef
Raffaele Valenti <raffaele-valenti@tutamail.com>
2023-06-18 17:22:05 +0200

    Localization update

--- src/loc/it-it.ts
+++ src/loc/it-it.ts
@@ -3,7 +3,7 @@ import { PortfolioLocalizations } from "@loc/mystrings"
 const itIT: PortfolioLocalizations = {
   landing: {
     description:
-      "Sono uno sviluppatore di software italiano autodidatta con una passione per lo sviluppo di videogiochi",
+      "Sono uno sviluppatore di software italiano, perlopiù autodidatta, con una passione per lo sviluppo di videogiochi",
     social: {
       email: "Email",
       github: "GitHub",
