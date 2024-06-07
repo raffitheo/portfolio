6b1970876162bcf1d87580a95d1be6439375e432
Raffaele Valenti <raffaele-valenti@protonmail.com>
2024-06-07 22:45:54 +0200

    Small localization improvement

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import i18n from '@localizations';
 import { StrictMode, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = lazy(() => import('./app.tsx'));
 
 import './index.sass';
+
+i18n.init();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
